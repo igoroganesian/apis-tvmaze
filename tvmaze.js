ddba2a8314e385b999129b93776cf9bfc386b461
Kirra Hyde <kirra.hyde@gmail.com>
2023-07-12 17:56:53 -0400

    did images

--- tvmaze.js
+++ tvmaze.js
@@ -15,32 +15,11 @@ const TVMAZE_URL = "http://api.tvmaze.com/search/shows";
 
 
 async function getShowsByTerm(term) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
-  // let $searchTerm = $searchForm.val();
+
   const searchResults = await axios.get(`${TVMAZE_URL}?q=${term}`);
   console.log(`the search result is: ${searchResults}`);
   return searchResults;
 
-
-
-  // return [
-  //   {
-  //     id: 1767,
-  //     name: "The Bletchley Circle",
-  //     summary:
-  //       `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-  //          women with extraordinary skills that helped to end World War II.</p>
-  //        <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-  //          normal lives, modestly setting aside the part they played in
-  //          producing crucial intelligence, which helped the Allies to victory
-  //          and shortened the war. When Susan discovers a hidden code behind an
-  //          unsolved murder she is met by skepticism from the police. She
-  //          quickly realises she can only begin to crack the murders and bring
-  //          the culprit to justice with her former friends.</p>`,
-  //     image:
-  //         "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-  //   }
-  // ]
 }
 
 
@@ -53,12 +32,18 @@ function displayShows(shows) {
   $showsList.empty();
   console.log('shows.data: ', shows.data[0]);
   for (const show of shows.data) {
+    let picUrl;
+    if (show.show.image) {
+      picUrl = show.show.image.medium;
+    } else {
+      picUrl = "https://tinyurl.com/tv-missing";
+    }
     const $show = $(`
          <div data-show-id="${show.show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
-              alt="Bletchly Circle San Francisco"
+              src="${picUrl}"
+              alt="Show image"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.show.name}</h5>
