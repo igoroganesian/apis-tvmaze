ee2879af5735999543b31e78225c85cee74acb4e
Kirra Hyde <kirra.hyde@gmail.com>
2023-07-12 17:41:06 -0400

    did displayShows

--- tvmaze.js
+++ tvmaze.js
@@ -52,28 +52,24 @@ async function getShowsByTerm(term) {
 function displayShows(shows) {
   $showsList.empty();
   console.log('shows.data: ', shows.data[0]);
-  debugger;
   for (const show of shows.data) {
-    const $show = $(`ShowID: ${show.id},
-                     Show name: ${show.name},
-                     Show summary: ${show.summary}`);
-    // const $show = $(`
-    //     <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
-    //      <div class="media">
-    //        <img
-    //           src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
-    //           alt="Bletchly Circle San Francisco"
-    //           class="w-25 me-3">
-    //        <div class="media-body">
-    //          <h5 class="text-primary">${show.name}</h5>
-    //          <div><small>${show.summary}</small></div>
-    //          <button class="btn btn-outline-light btn-sm Show-getEpisodes">
-    //            Episodes
-    //          </button>
-    //        </div>
-    //      </div>
-    //    </div>
-    //   `);
+    const $show = $(`
+         <div data-show-id="${show.show.id}" class="Show col-md-12 col-lg-6 mb-4">
+         <div class="media">
+           <img
+              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              alt="Bletchly Circle San Francisco"
+              class="w-25 me-3">
+           <div class="media-body">
+             <h5 class="text-primary">${show.show.name}</h5>
+             <div><small>${show.show.summary}</small></div>
+             <button class="btn btn-outline-light btn-sm Show-getEpisodes">
+               Episodes
+             </button>
+           </div>
+         </div>
+       </div>
+      `);
 
     $showsList.append($show);
   }
