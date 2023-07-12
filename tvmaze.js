1bbb82f29ae7d67860a7bd220a82c756139f8c80
Igor Oganesian <igor.oganesian@gmail.com>
2023-07-12 15:32:55 -0400

    working on displayShows()

--- tvmaze.js
+++ tvmaze.js
@@ -51,25 +51,29 @@ async function getShowsByTerm(term) {
 
 function displayShows(shows) {
   $showsList.empty();
-
-  for (const show of shows) {
-    const $show = $(`
-        <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
-         <div class="media">
-           <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
-              alt="Bletchly Circle San Francisco"
-              class="w-25 me-3">
-           <div class="media-body">
-             <h5 class="text-primary">${show.name}</h5>
-             <div><small>${show.summary}</small></div>
-             <button class="btn btn-outline-light btn-sm Show-getEpisodes">
-               Episodes
-             </button>
-           </div>
-         </div>
-       </div>
-      `);
+  console.log('shows.data: ', shows.data[0]);
+  debugger;
+  for (const show of shows.data) {
+    const $show = $(`ShowID: ${show.id},
+                     Show name: ${show.name},
+                     Show summary: ${show.summary}`);
+    // const $show = $(`
+    //     <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+    //      <div class="media">
+    //        <img
+    //           src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+    //           alt="Bletchly Circle San Francisco"
+    //           class="w-25 me-3">
+    //        <div class="media-body">
+    //          <h5 class="text-primary">${show.name}</h5>
+    //          <div><small>${show.summary}</small></div>
+    //          <button class="btn btn-outline-light btn-sm Show-getEpisodes">
+    //            Episodes
+    //          </button>
+    //        </div>
+    //      </div>
+    //    </div>
+    //   `);
 
     $showsList.append($show);
   }
